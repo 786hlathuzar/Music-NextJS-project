0f874cb5bf930109a0fb81f614c5671c0bc1bfd1
786hlathuzar <hlathuzar786786@gmail.com>
2024-07-27 15:41:20 +0630

    text change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
     <div className="h-auto md:h-[40rem] w-full flex flex-col rounded-md items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 text-center">
       <div className="p-4 relative z-10 w-full  text-center font-light">
         <h1>Master the art of music</h1>
-        <p>
+        <p style={{ color: "white" }}>
           Drive into our comprehensive music courses and transform your musical
           journey today.Whether you're a begineer or looking to refine your
           skills,join us to unlock your true potentail
